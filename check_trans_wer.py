d9cef8f851283abe4eba0b052fd90c9b16f7edd4
kaousheik26 <kaousheik@example.com>
2025-01-26 15:35:10 +0530

    count

--- check_trans_wer.py
+++ check_trans_wer.py
@@ -28,15 +28,24 @@ with open(ref) as f:
 with open(hyp) as f:
     hyp_lines = f.readlines()
 lines = []
+total_english_count = 0
+error_count = 0
 for ref_line, hyp_line in zip(ref_lines, hyp_lines):
     
     for word_ref, word_hyp in zip(ref_line.strip().split(" "), hyp_line.strip().split(" ")):
+        if word_hyp == word_ref:
+            lang = identify_lang(word_ref):
+            if lang == 'english':
+                total_english_count += 1
         if word_hyp != word_ref:
             lang = identify_lang(word_ref)
             if lang == 'english':
+                total_english_count += 1
                 print(word_hyp, word_ref)
+                error_count += 1
             lines.append(f"{word_ref} {word_hyp} /n")
-                
+            
 
+print(error_count, total_english_count)
 with open(f"{sys.argv[1]}/trans-errors", "w") as f:
     f.writelines(lines)
