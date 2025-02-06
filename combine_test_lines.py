837a446403083d8c9e0a7fa3fb8aabf619643e6d
kaousheik26 <kaousheik@example.com>
2025-02-06 10:54:07 +0530

    added combined lines

---
+++ combine_test_lines.py
@@ -0,0 +1,32 @@
+import os
+import sys
+full_path = sys.argv[1]
+hyp = 'hyp.txt'
+ref = 'ref.txt'
+org = 'large/eval-org.cls'
+
+with open(os.path.join(full_path, hyp)) as f:
+    hyp_lines = f.readlines()
+    
+with open(os.path.join(full_path, ref)) as f:
+    ref_lines = f.readlines()
+    
+with open(org) as f:
+    org_lines = f.readlines()
+
+final_hyp = []
+final_ref = []
+count = 0
+for line in org_lines:
+    length = len(line.strip().split(" "))
+    h = ' '.join(hyp_lines[count:length])
+    r = ' '.join(ref_lines[count:length])
+    final_hyp.append(h)
+    final_ref.append(r)
+    count += length
+
+with open(os.path.join(full_path, "hyp"), "w" ) as f:
+    f.writelines(final_hyp)
+
+with open(os.path.join(full_path, "ref"), "w") as f:
+    f.writelines(final_ref)
